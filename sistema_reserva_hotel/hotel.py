f482b88825212411b5ab45b1f5f92940f98e34a0
Lívia Menato <liviamenato@gmail.com>
2024-11-12 14:40:25 -0300

    visualizacao quartos ocupados

--- sistema_reserva_hotel/hotel.py
+++ sistema_reserva_hotel/hotel.py
@@ -1,6 +1,4 @@
 # Gerencia o hotel e sua lista de quartos e reservas
-
-
 class Hotel:
     def __init__(self, nome, endereco):
         self.nome = nome
@@ -10,3 +8,12 @@ class Hotel:
 
     def armazenar_quarto(self, quarto):
         self.quarto.append(quarto)
+        print(f"Quarto {quarto.numero} ({quarto.tipo})adicionado ao hotel {self.nome}.")
+
+    def listar_quartos_disponiveis(self):
+        print(f"Quartos disponíveis {self.nome}:")
+        for quarto in self.quarto:
+            if quarto.verificar_disponibilidade():
+                print(f"Quarto {quarto.numero}: Disponível")
+            else:
+                print(f"Quarto {quarto.numero}: Ocupado")
--- sistema_reserva_hotel/main.py
+++ sistema_reserva_hotel/main.py
@@ -12,3 +12,16 @@ hotel = Hotel("Hotel XYZ", "Rua 123, Cidade ABC")
 # Armazenando os quartos no hotel
 hotel.armazenar_quarto(quarto1)
 hotel.armazenar_quarto(quarto2)
+
+# listando os quartos do hotel
+hotel.listar_quartos_disponiveis()
+
+# alterando status do quarto e verificando sua disponibilidade
+quarto1.marcar_como_ocupado()
+print(
+    f"Quarto {quarto1.numero} disponível?\nO quarto está {'Disponível' if quarto1.verificar_disponibilidade() else 'Ocupado'}"
+)
+
+
+# listando os novos quartos disponiveis
+hotel.listar_quartos_disponiveis()
